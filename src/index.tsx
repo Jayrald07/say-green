334f570d698ba84c5134a6c8d1d88d65b1698ad7
Jayrald07 <geraldempino@gmail.com>
2024-01-14 11:12:20 +0800

    Source code fix
    - Use const for variables that did not reassigned

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Plotter from "./pages/plotter";
 import "./index.css";
-let root = createRoot(document.getElementById("root") as Element);
+const root = createRoot(document.getElementById("root") as Element);
 
 root.render(<Plotter />);

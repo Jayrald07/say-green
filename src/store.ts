0f15011dc4c2c613c225a1b6195bac380b3ccb5f
Jayrald07 <geraldempino@gmail.com>
2024-01-14 10:49:43 +0800

    Update context provider
    - Delete store file since it is unnecessary

--- src/store.ts
+++
@@ -1,7 +0,0 @@
-import { Map } from "mapbox-gl";
-import { createContext } from "react";
-
-export const MapContext = createContext<Map | null>(null);
-export const CurrentActionContext = createContext<{ type: string } | null>(
-  null
-);
